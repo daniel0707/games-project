7df3a641a61718d4ecfc4001a874eec503676a09
daniel0707 <vahla.daniel@gmail.com>
2017-02-28 11:52:53 +0200

    Movement is now force based

---
+++ UnityProject/Assets/DBscript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+
+public class DBscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string conn = "URI=file:" + Application.dataPath + "/DB/Database.db"; //Path to database.
+		IDbConnection dbconn;
+		dbconn = (IDbConnection) new SqliteConnection(conn);
+		dbconn.Open(); //Open connection to the database.
+		IDbCommand dbcmd = dbconn.CreateCommand();
+		string sqlQuery = "SELECT Rank,Name,Score FROM HighScore";
+		dbcmd.CommandText = sqlQuery;
+		IDataReader reader = dbcmd.ExecuteReader();
+		while (reader.Read())
+		{
+			int Rank = reader.GetInt32(0);
+			string Name = reader.GetString(1);
+			int Score = reader.GetInt32(2);
+
+			Debug.Log( "Rank= "+Rank+"  Name= "+Name+"  Score= "+  Score);
+		}
+		reader.Close();
+		reader = null;
+		dbcmd.Dispose();
+		dbcmd = null;
+		dbconn.Close();
+		dbconn = null;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- UnityProject/Assets/MovePenguin.cs
+++ UnityProject/Assets/MovePenguin.cs
@@ -4,39 +4,48 @@ using UnityEngine;
 
 public class MovePenguin : MonoBehaviour {
 
-	private float verticalSpeed = 0.0f;
-	private float jumpSpeed = 50.0f;
-	private float horizontalSpeed = 0.0f;
+	private float jumpSpeed = 2160000f;
+	private float comboSpeed = 0.3f;
+	private float maxSpeed = 100f;
+	private float minSpeed = -100f;
+	private float sideSpeed = 10f;
+	private bool isOnFloor = false;
+	private Rigidbody2D penguinBody;
 
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Floor") {
+			isOnFloor = true;
+		}
+	}
 	private void movement(){
-		if (Input.GetKey (KeyCode.UpArrow)) { //detect if the key down or up 
-			verticalSpeed = jumpSpeed;
-			//transform.Translate ( 0,jumpSpeed * Time.deltaTime,0);
+		if (Input.GetKey (KeyCode.UpArrow) && isOnFloor) { //detect if the key down or up and is on floor
+			penguinBody.AddForce(Vector2.up * jumpSpeed * comboSpeed);
+			//transform.Translate(0,jumpSpeed * Time.deltaTime,0);
+			isOnFloor = false;
 		}
+
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			horizontalSpeed = -20;
-			//transform.Translate ( horizontalSpeed * Time.deltaTime,0,0);
-		}
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			horizontalSpeed = 20;
-			//transform.Translate ( horizontalSpeed * Time.deltaTime,0,0);
-		}
-
-
-		verticalSpeed -= 1f;
-
-		if (horizontalSpeed > 0) {
-				horizontalSpeed -= 0.5f;
+			if (penguinBody.velocity.x > minSpeed) {
+				penguinBody.velocity = new Vector2 (penguinBody.velocity.x - sideSpeed, penguinBody.velocity.y);
+			} else {
+				penguinBody.velocity = new Vector2 (minSpeed, penguinBody.velocity.y);
 			}
-		if (horizontalSpeed < 0) {
-			horizontalSpeed += 0.5f;
 		}
-			transform.Translate(horizontalSpeed* Time.deltaTime,verticalSpeed* Time.deltaTime,0);
+				
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			if (penguinBody.velocity.x < maxSpeed) {
+				penguinBody.velocity = new Vector2 (penguinBody.velocity.x + sideSpeed, penguinBody.velocity.y);
+			} else {
+				penguinBody.velocity = new Vector2 (maxSpeed, penguinBody.velocity.y);
+			}
 		}
+					
+	}
 
 	// Use this for initialization
 	void Start () {
-		
+		penguinBody = this.gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
